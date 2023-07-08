dc86125e89e2d874d85b39dd07773f7a8ac415e0
Akash Kumar <aakashguptarbs@gmail.com>
2023-07-09 01:05:01 +0530

    Second file updated

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,30 @@ import Common from './Commmon';
 
 function About() {
   return (
-    <Common name='Welcome to About page' imgsrc={web} visit='/contact' btname="Contact now"/>
+    <>
+    
+    <Common name='Welcome to About page' imgsrc={web} visit='/contact' btname="Contact now"/><br /><br />
+    <h2>What is Buissness Growth?</h2>
+        <p>Developing a business growth plan can help you identify and implement strategies to boost revenue, expand your customer base or create new products. Knowing what a business growth plan is and the best practices for creating one can help you successfully grow a company. It can also help you create more effective plans to motivate teams and encourage goal-setting practices.</p><br />
+        <br />
+        <h2>Strategies for business growth</h2>
+        <p>ou can use a variety of strategies to help you create your own business growth plan, depending on your goals and the methods you want to use to meet them. A few of the most common types of business growth strategies include:</p>
+        <ul>
+            <li><h3>Market development strategy</h3></li>
+            <p>A market development strategy focuses on increasing growth by assessing and updating your marketing efforts to target new demographics. Using this strategy, you identify new markets to target through advertising and marketing. For example, a marketing department may create targeted advertisements for a new geographic location to reach a wider audience.</p>
+
+            <li><h3>Product development strategy</h3></li>
+            <p>A product development strategy involves creating new products or services to serve your current target market. Rather than trying to reach new customers, your focus is on developing new items for your current customers to increase sales and continue to grow.
+
+For example, a home decor company may create a new line of candles to expand its product offerings. Sometimes, companies may decide to modify existing products to make them appear to be new or upgraded offerings. This may include changing the labeling, packaging or appearance of the product.</p>
+        <li><h3>Diversification strategy</h3></li>
+        <p>Diversification is a combination of market and product development strategies. When diversifying, instead of focusing on a single endeavor, you consider new products and services you might offer to new markets. For example, a health food store may develop a line of organic skincare products to reach a new market.
+
+Another example is a fitness store that solely offers home fitness equipment that decides to offer athletic shoes and workout clothing lines as well. This diversification can lead to far more sales from current home fitness equipment customers. In this case, the fitness store offers items that keep its current customers coming back on a regular basis. Before this diversification, the current customers were purchasing home fitness equipment that didn't need replacement on a short-term ongoing basis, as athletic shoes and workout clothes do.</p>
+        </ul>
+
+    </>
+
   )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,22 @@ import web from '../src/images/pic2.png'
 import Common from './Commmon';
 function Home() {
     return (
-        <Common name="Grow your Buissness with" imgsrc={web} visit='/service' btname="Get Started"/>
+        <>
+         <Common name="Grow your Buissness with" imgsrc={web} visit='/service' btname="Get Started"/><br /><br />
+        <h2>Tips for creating an effective business growth plan</h2>
+        <p>Use these tips to help you create a comprehensive and actionable business growth plan for an organization:</p>
+        <ul>
+            <li><h3>Know the company's strengths</h3></li>
+            <p>Ensure your growth strategy aligns with areas in which the company excels. Use these talents to help you identify areas where the company can grow organically.</p>
+            <li><h3>Make your next growth plan</h3></li>
+            <p>Before you reach the end of your current growth strategy, begin brainstorming what your next growth goal might be. Make sure you're consistently working toward a growth objective.</p>
+            <li><h3>Review other plans</h3></li>
+            <p>You can often gain insights from other businesses to help you develop a growth strategy. Search for other companies' growth plans so you can determine what templates you might consider and other details you can use for your own goals.</p>
+            <li><h3>Use your team</h3></li>
+            <p>Make sure you have skilled team members who can help you develop and implement a business growth plan. Seek their input to help you identify areas where you can improve the plan to achieve success by scheduling a regular meeting with the team to discuss new ideas, concerns and growth success.</p>
+        </ul>
+        </>
+
     )
 }
 
